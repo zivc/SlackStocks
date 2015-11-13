a794019897c5b8bf6ede764c9acdb5b446ed1307
Taylor, Ash <ashley.taylor@sungard.com>
2015-11-13 17:10:25 +0000

    thing

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.use(require('body-parser').urlencoded({ extended: true }));
 
 // test route
 app.post('/hello', function (req, res, next) {
+	console.log(req.body);
 	var userName = req.body.user_name,
 		botPayload = {
 			text : 'Hello, ' + userName + '!'
